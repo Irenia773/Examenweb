7ab28a2a653e6abf2efe8466b97669c8e57c3fa0
14171465@itculiacan.edu.mx <14171465@itculiacan.edu.mx>
2020-05-18 01:48:11 -0600

    Commit con archivos base del examen

---
+++ ejemploexamen2020-master/Models/Leche.cs
@@ -0,0 +1,14 @@
+
+
+namespace ejemplo.Models
+ {
+     public class Leche
+     {
+         public int Id { get; set; }
+         public string Nombre { get; set; }
+        public string Marca { get; set; }  
+         public int Etapa { get; set; }
+         
+     }
+
+}
