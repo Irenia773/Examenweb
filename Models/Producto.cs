bbca705dfca41bbff242e478ebb3287895a49a72
14171465@itculiacan.edu.mx <14171465@itculiacan.edu.mx>
2020-05-18 22:32:11 -0600

    paso 5 se agrego el almacen de datos en el repository

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -6,9 +6,9 @@ namespace ejemplo.Models
      {
          public int Id { get; set; }
          public string Nombre { get; set; }
-        public string Linea { get; set; }  
+        public string Linea { get; set; } 
          public decimal Precio { get; set; }
          
      }
 
-}
+} 
---
+++ ProductosRepository.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Dapper;
+using ejemplo.Models;
+using Microsoft.Data.Sqlite;
+
+namespace ejemplo {
+
+
+public class ProductosRepository {
+
+    public ProductosRepository()
+    {
+        
+    }
+    private const string DBCON = @"Data Source=mydb.db;";
+    public static void IniciarBD(){
+        using (var connection = new SqliteConnection(DBCON))
+//        ("" +    new SqliteConnectionStringBuilder()  {        DataSource = "data.db"  }))
+        {
+            connection.Open();
+            connection.Execute(
+                @"CREATE TABLE IF NOT EXISTS Productos (
+                        Id INT PRIMARY KEY,
+                        Nombre TEXT NULL,
+                        Linea TEXT NULL,
+                        Precio DECIMAL NULL
+                ) ;");
+        }
+    }
+
+        internal List<Leche> LeerTodos()
+        {
+            using(var con = new SqliteConnection(DBCON)){
+
+                return con.Query<Leche>("SELECT Id, Nombre, Marca, Etapa FROM Leches ").ToList();
+            }
+        }
+
+        internal Leche LeerPorId(int id)
+        {
+            using(var con = new SqliteConnection(DBCON)){
+                return con.Query<Leche>("SELECT Id, Nombre, Marca, Etapa FROM Leches WHERE Id = @Id "
+                    , new { Id = id }).FirstOrDefault();
+            }
+        }
+
+        internal void Crear(Leche model)
+        {
+            using(var con = new SqliteConnection(DBCON)){
+                con.Execute("INSERT INTO Leches ( Nombre, Marca, Etapa) VALUES ( @Nombre, @Marca, @Etapa ) "
+                    , model);
+            }
+        }
+
+        internal void Actualizar(Leche model)
+        {
+             using(var con = new SqliteConnection(DBCON)){
+                 con.Execute("UPDATE Leches SET  Nombre = @Nombre , Marca = @Marca, Etapa = @Etapa WHERE Id = @Id "
+                    , model);
+            }
+        }
+
+        internal void Borrar(int id)
+        {
+            using(var con = new SqliteConnection(DBCON)){
+                con.Execute("DELETE FROM Leches WHERE Id = @Id "
+                    , new { Id = id });
+            }
+        }
+    }
+
+
+}
