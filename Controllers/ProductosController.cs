189effc21e06d1125c28f253de48a23b0df28650
14171465@itculiacan.edu.mx <14171465@itculiacan.edu.mx>
2020-05-18 22:21:24 -0600

    IMNR paso 4 creacion del repositorio de productos en blanco

---
+++ Controllers/ProductosController.cs
@@ -0,0 +1,74 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ejemplo.Models;
+
+namespace ejemplo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductosController : ControllerBase
+    {
+        LechesRepository repositorio;
+        public ProductosController()
+        {
+            repositorio = new LechesRepository();
+        }
+        // GET: api/Leches
+        [HttpGet]
+        public ActionResult<List<Leche>> Get()
+        {
+            var todos = repositorio.LeerTodos();
+            return todos;
+        }
+
+        // GET: api/Leches/5
+        [HttpGet("{id}", Name = "Get")]
+        public ActionResult<Leche> Get(int id)
+        {
+            var leche = repositorio.LeerPorId(id);
+            if(leche == null){
+                return NotFound();
+            }
+            return leche;
+        }
+
+        // POST: api/Leches
+        [HttpPost]
+        public IActionResult Post([FromBody] Leche model)
+        {
+            repositorio.Crear(model);
+
+            return Ok();
+        }
+
+        // PUT: api/Leches/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Leche model)
+        {
+            var leche = repositorio.LeerPorId(model.Id);
+            if(leche == null){
+                return NotFound();
+            }
+            
+            repositorio.Actualizar(model);
+            return Ok();
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var leche = repositorio.LeerPorId(id);
+            if(leche == null){
+                return NotFound();
+            }
+            repositorio.Borrar(id);
+            return Ok();
+        }
+    }
+}
