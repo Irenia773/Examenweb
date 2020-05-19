cc73107342829c3a1ff22bd5a08c292f683902d7
14171465@itculiacan.edu.mx <14171465@itculiacan.edu.mx>
2020-05-18 22:03:53 -0600

    paso 2 creacion del modelo terminado

---
+++ Models/Producto.cs
@@ -0,0 +1,14 @@
+
+
+namespace ejemplo.Models
+ {
+     public class Producto
+     {
+         public int Id { get; set; }
+         public string Nombre { get; set; }
+        public string Linea { get; set; }  
+         public decimal Precio { get; set; }
+         
+     }
+
+}
